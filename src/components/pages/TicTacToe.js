fb64ce315372fdae383ed281af13e834d690d88d
Sebs5384 <operationmadjackal@hotmail.com>
2024-04-10 23:02:31 -0300

    Replaces array for a matrix and updates TicTacToe file
    Additionally it will add a new component "TicTacToeBoard" which consist on the same 3x3 board but by using our matrix

--- src/components/pages/TicTacToe.js
+++ src/components/pages/TicTacToe.js
@@ -34,44 +34,62 @@ WinnerCard.propTypes = {
 
 const getWinner = tiles => {
   const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ]
+    [[0, 0], [0, 1], [0, 2]],
+    [[1, 0], [1, 1], [1, 2]],
+    [[2, 0], [2, 1], [2, 2]],
+    [[0, 0], [1, 0], [2, 0]],
+    [[0, 1], [1, 1], [2, 1]],
+    [[0, 2], [1, 2], [2, 2]],
+    [[0, 0], [1, 1], [2, 2]],
+    [[0, 2], [1, 1], [2, 0]]
+  ];
 
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (tiles[a] && tiles[a] === tiles[b] && tiles[a] === tiles[c]) {
-      return tiles[a];
-    }
-  }
+  for(const line of lines) {
+    const [a, b, c] = line;
+    const [rowA, columnA] = a;
+    const [rowB, columnB] = b;
+    const [rowC, columnC] = c;
+
+    if(tiles[rowA][columnA] && tiles[rowA][columnA] === tiles[rowB][columnB] && tiles[rowA][columnA] === tiles[rowC][columnC]) {
+      return tiles[rowA][columnA];
+    };    
+  };
 
   return null;
 };
 
+const getMatrix = (lengthOfMatrix, value) => {
+  const matrix = [];
+  for(let i = 0; i < lengthOfMatrix; i++) {
+    const row = Array(lengthOfMatrix).fill(value);
+    matrix.push(row);
+  };
+
+  return matrix;
+}
+
 const useTicTacToeGameState = initialPlayer => {
-  const [tiles, setTiles] = React.useState(Array(9).fill(''));
+  const table = getMatrix(3, '');
+  const [tiles, setTiles] = React.useState(table);
   const [currentPlayer, setCurrentPlayer] = React.useState(initialPlayer);
   const winner = getWinner(tiles);
   const [gameEnded, setGameEnded] = React.useState(false);
 
-  const setTileTo = (tileIndex, player) => {
-    if(tiles[tileIndex] !== '') {
+  const setTileTo = (rowIndex, columnIndex, player) => {
+    if(tiles[rowIndex][columnIndex] !== '') {
       return;
     };
 
-    const newTiles = [...tiles];
-    newTiles[tileIndex] = player;
+    const newTiles = tiles.map((row, index) => 
+      index === rowIndex ? row.map((value, index) => 
+      index === columnIndex ? player : value) : row
+    );
+
     setTiles(newTiles);
     setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X');
   };
   const restart = () => {
-    setTiles(Array(9).fill(''));
+    setTiles(getMatrix(3, ''));
     setCurrentPlayer(initialPlayer);
     setGameEnded(false);
   };
@@ -79,26 +97,30 @@ const useTicTacToeGameState = initialPlayer => {
   return { tiles, currentPlayer, winner, gameEnded, setTileTo, restart };
 };
 
+const TicTacToeBoard = ({ tiles, setTileTo, currentPlayer }) => {
+  return (
+    <>
+      {tiles.map((row, rowIndex) => (
+        <div key={rowIndex} className="tictactoe-row">
+          {row.map((tile, colIndex) => (
+            <Square
+              key={rowIndex * 3 + colIndex}
+              value={tile}
+              onClick={() => setTileTo(rowIndex, colIndex, currentPlayer)}
+            />
+          ))}
+        </div>
+      ))}
+    </>
+  );
+};
+
 const TicTacToe = () => {
   const { tiles, currentPlayer, winner, gameEnded, setTileTo, restart } = useTicTacToeGameState('X');
   return (
     <div className="tictactoe">
       <WinnerCard show={winner} winner={winner} onRestart={() => {restart()}} />
-      <div className="tictactoe-row">
-        <Square key={0} value={tiles[0]} onClick={() => {setTileTo(0, currentPlayer)}} />
-        <Square key={1} value={tiles[1]} onClick={() => {setTileTo(1, currentPlayer)}} />
-        <Square key={2} value={tiles[2]} onClick={() => {setTileTo(2, currentPlayer)}} />
-      </div>
-      <div className="tictactoe-row">
-        <Square key={3} value={tiles[3]} onClick={() => {setTileTo(3, currentPlayer)}} />
-        <Square key={4} value={tiles[4]} onClick={() => {setTileTo(4, currentPlayer)}} />
-        <Square key={5} value={tiles[5]} onClick={() => {setTileTo(5, currentPlayer)}} />
-      </div>
-      <div className="tictactoe-row">
-        <Square key={6} value={tiles[6]} onClick={() => {setTileTo(6, currentPlayer)}} />
-        <Square key={7} value={tiles[7]} onClick={() => {setTileTo(7, currentPlayer)}} />
-        <Square key={8} value={tiles[8]} onClick={() => {setTileTo(8, currentPlayer)}} />
-      </div>
+      <TicTacToeBoard tiles={tiles} setTileTo={setTileTo} currentPlayer={currentPlayer} />
     </div>
   );
 };
